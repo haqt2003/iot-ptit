3d817f3f272d35d34ed12a4cb42bf7733b4b6200
Tran Ha <haqt2003@gmail.com>
2025-02-12 13:49:51 +0700

    update profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -34,23 +34,45 @@
           />
         </div>
       </div>
-      <div class="mt-[48px] w-full glassmorphism h-[734px] p-10">
-        <div class="flex gap-[60px]">
-          <img src="../assets/images/img.svg" alt="" />
-          <div class="text-[20px]">
-            <div class="mt-6">
-              <span class="font-bold">Họ và tên: </span>Trần Quang Hà
+      <div class="mt-[48px] w-full glassmorphism h-[720px] p-10">
+        <div class="flex justify-between gap-10">
+          <div class="h-[650px]">
+            <div class="rounded-[24px] h-[400px] w-[400px] overflow-hidden">
+              <img src="../assets/images/img.svg" alt="" />
             </div>
-            <div class="mt-6">
-              <span class="font-bold">Mã sinh viên: </span>B21DCPT007
+            <div class="text-[20px]">
+              <div class="mt-6 font-bold text-[32px] text-center gap-4">
+                Trần Quang Hà
+              </div>
+              <div class="mt-2 text-center">B21DCPT007</div>
+              <div class="mt-2 text-center font-light">haqt2003@gmail.com</div>
+              <div class="flex mt-8 items-center justify-center gap-4">
+                <a
+                  href="https://github.com/haqt2003/iot-ptit"
+                  target="_blank"
+                  class="flex glassmorphism items-center justify-center gap-4 w-[160px] py-2"
+                >
+                  <img src="../assets/images/github-icon.svg" alt="" />
+                  <span>Github</span>
+                </a>
+                <a
+                  href="https://github.com/haqt2003/iot-ptit"
+                  target="_blank"
+                  class="flex glassmorphism items-center justify-center gap-3 w-[160px] py-2"
+                >
+                  <img src="../assets/images/web-icon.svg" alt="" />
+                  <span>API Docs</span>
+                </a>
+              </div>
             </div>
-            <a
-              href="https://github.com/haqt2003/iot-ptit"
-              target="_blank"
-              class="mt-6 block"
-            >
-              <span class="font-bold">Github: </span>github.com/haqt2003/io
-            </a>
+          </div>
+          <div class="rounded-[24px] overflow-hidden w-[720px] h-[640px]">
+            <object
+              data="https://firebasestorage.googleapis.com/v0/b/the-best-records.appspot.com/o/tmp.pdf?alt=media&token=5e76b706-b4b6-438a-9652-1fd842c76d2a"
+              type="application/pdf"
+              width="100%"
+              height="100%"
+            ></object>
           </div>
         </div>
       </div>
